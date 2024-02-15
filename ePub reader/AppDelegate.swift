f0070b059a519a8c13c8474853bd089f135dbbb5
Akhmedau <akhmiedov.199629@gmail.com>
2024-02-15 17:57:36 +0100

    first commit

---
+++ ePub reader/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ePub reader
+//
+//  Created by АХМЕДОВ НИКОЛАЙ on 14/02/2024.
+//  Copyright © 2024 Ahmedov Nikolay. All rights reserved.
+//
+
+import UIKit
+import FolioReaderKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
